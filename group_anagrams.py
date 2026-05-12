5bbea16e8ceb1a4779f3d462fc5fa0de0cec8026
Daniel <daniel.lukins1@gmail.com>
2026-05-13 08:25:41 +1000

    yes

---
+++ group_anagrams.py
@@ -0,0 +1,26 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+
+        # make hashmap of sorted str
+        # if the sorted str is in the hashamp, then append it to the list in that hashamp
+        map = {}
+        res = []
+
+        for i in strs:
+            if "".join(sorted(i)) not in map:
+                map["".join(sorted(i))] = [i]
+            else:
+                map["".join(sorted(i))].append(i)
+
+        for i in map.values():
+            res.append(i)
+        return res
+
+
+
+
+
+solution = Solution()
+print(solution.groupAnagrams(["act","pots","tops","cat","stop","hat"]))
+
+
