b733770834ca4edea138f35106af7d66dc83a1e9
Daniel <daniel.lukins1@gmail.com>
2026-05-16 09:51:52 +1000

    twosum w hashmap completed

---
+++ sort_an_array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        res = [nums[0]]
+        for i in range(1,len(nums)):
+            inserted = False
+            for j in range(len(res)): 
+                if res[j] == nums[i] or nums[i] < res[j]:
+                    res.insert(j, nums[i])
+                    inserted = True
+                    break
+            if not inserted:
+                res.append(nums[i])
+
+                
+        
+        return res
+    
+
+
+solution = Solution()
+print(solution.sortArray([10,9,1,1,1,2,3,1]))
+
+
+
+
+
+
