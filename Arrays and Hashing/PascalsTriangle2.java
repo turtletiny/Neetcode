336f58dee9ad104fa278b7731cbcc4db3abef927
Daniel <daniel.lukins1@gmail.com>
2026-06-23 08:50:31 +1000

    stuff

---
+++ Arrays and Hashing/PascalsTriangle2.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        int[] res = new int[rowIndex+1];
+        res[0] = res[rowIndex+2] = 1;
+
+        for (int i = 0; i < rowIndex + 1; i++){
+
+        }
+
+
+    }
+}
